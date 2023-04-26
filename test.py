852f61fcba0765784e46eada39390d22327925a7
masoud89git config --global user.email m.lakestani89@gmail.com <m.lakestani89@gmail.comgit config --global user.name masoud89>
2023-04-26 15:38:28 +0330

    delete unused file

--- test.py
+++
@@ -1,5 +0,0 @@
-signup_message = "\n\t\t\t\t\t\tAre you ready to challenge your knowledge?\n\n\
-\t\t\t\t\t\tSo, wait no more and sign up.\n\n\
-Please note that in order to sign up as an admin (superuser), you must enter the security code. Don't worry if you don't have a security code, you can still sign up as a regular user and enjoy the quiz."
-
-print(signup_message)
